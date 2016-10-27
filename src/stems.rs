d0127c6e50d72ec2bb04518e524d5e0feb53af7a
Volker Mische <volker.mische@gmail.com>
2016-10-27 15:39:44 -0700

    Improved stemming
    The stemming code now does:
    
     1. Lowercasing
     2. Unicode normalization
     3. Porter stemming
    
    The tokens are split on Unicode word boundaries.

--- src/stems.rs
+++ src/stems.rs
@@ -1,11 +1,15 @@
 extern crate stemmer;
+extern crate unicode_normalization;
+extern crate unicode_segmentation;
+
 
 use self::stemmer::Stemmer;
+use self::unicode_normalization::UnicodeNormalization;
+use self::unicode_segmentation::UnicodeSegmentation;
 
 
-//#[derive(Debug)]
 pub struct Stems<'a> {
-    text: &'a str,
+    words: unicode_segmentation::UnicodeWordIndices<'a>,
     // The current cursor position
     current: usize,
     stemmer: Stemmer,
@@ -17,9 +21,10 @@ pub struct StemmedWord {
     pub stemmed_offset: usize,
     // Where the suffix starts
     pub suffix_offset: usize,
-    // The length of the suffix
-    suffix_len: usize,
+    // The stemmed word
     pub stemmed: String,
+    // The difference between the stemmed word and the original lowercased one. It can be
+    // used to recontruct the original word (for exact match searches)
     pub suffix: String,
 }
 
@@ -27,7 +32,7 @@ pub struct StemmedWord {
 impl<'a> Stems<'a> {
     pub fn new(text: &str) -> Stems {
         Stems{
-            text: text,
+            words: text.unicode_word_indices(),
             current: 0,
             stemmer: Stemmer::new("english").unwrap(),
         }
@@ -50,48 +55,21 @@ impl<'a> Iterator for Stems<'a> {
     type Item = StemmedWord;
 
     fn next(&mut self) -> Option<StemmedWord> {
-        let text = &self.text[self.current..];
-        let mut word_start = 0;
-        let mut word_len = 0;
-        for cc in text.chars() {
-            // A character that is part of a word we want to stem
-            if cc.is_alphabetic() {
-                word_len += cc.len_utf8();
-            }
-            else {
-                // There might be characters we don't care about for stemming
-                // (whitespace and numbers) before the word
-                if word_len == 0 {
-                    word_start += cc.len_utf8();
-                }
-                // We've reached the end of the word
-                else  {
-                    break;
-                }
-            }
-        }
-        // No word found
-        if word_len == 0 {
-            None
-        } else {
-            // Do the actual stemming
-            let input = text[word_start..word_start + word_len].to_lowercase();
-            // The lowercase version might contain more bytes than the uppercase one:
-            // e.g. İ => i̇
-            let lowercase_len = input.len();
-            let stemmed = self.stemmer.stem(&input);
-            let prefix_len = Stems::common_prefix_len(&stemmed, &input);
-            println!("{} {} {}", input, stemmed, prefix_len);
-
-            let ret = StemmedWord {
-                stemmed_offset: self.current + word_start,
-                suffix_offset: self.current + word_start + prefix_len,
-                suffix_len: lowercase_len - prefix_len,
-                stemmed: stemmed,
-                suffix: (&input[prefix_len..lowercase_len]).to_string(),
-            };
-            self.current += word_start + word_len;
-            Some(ret)
+        match self.words.next() {
+            Some((pos, word)) => {
+                let lowercased = word.to_lowercase();
+                let normalized = lowercased.nfkc().collect::<String>();
+                let stemmed = self.stemmer.stem(&normalized);
+                let prefix_len = Stems::common_prefix_len(&stemmed, &normalized);
+                let ret = StemmedWord {
+                    stemmed_offset: pos,
+                    suffix_offset: pos + prefix_len,
+                    stemmed: stemmed,
+                    suffix: (&normalized[prefix_len..normalized.len()]).to_string(),
+                };
+                Some(ret)
+            },
+            None => None
         }
     }
 
@@ -108,20 +86,20 @@ mod tests {
         let stems = Stems::new(input);
         let result = Stems::new(input).collect::<Vec<StemmedWord>>();
         let expected = vec![
-            StemmedWord { stemmed_offset: 0, suffix_offset: 5, suffix_len: 0,
+            StemmedWord { stemmed_offset: 0, suffix_offset: 5,
                           stemmed: String::from("these"), suffix: String::from("") },
-            StemmedWord { stemmed_offset: 6, suffix_offset: 10, suffix_len: 1,
+            StemmedWord { stemmed_offset: 6, suffix_offset: 10,
                           stemmed: String::from("word"), suffix: String::from("s") },
             // "deeply" stems to "deepli"
-            StemmedWord { stemmed_offset: 12, suffix_offset: 17, suffix_len: 1,
+            StemmedWord { stemmed_offset: 12, suffix_offset: 17,
                           stemmed: String::from("deepli"), suffix: String::from("y") },
-            StemmedWord { stemmed_offset: 19, suffix_offset: 23, suffix_len: 0,
+            StemmedWord { stemmed_offset: 19, suffix_offset: 23,
                           stemmed: String::from("test"), suffix: String::from("") },
-            StemmedWord { stemmed_offset: 24, suffix_offset: 30, suffix_len: 2,
+            StemmedWord { stemmed_offset: 24, suffix_offset: 30,
                           stemmed: String::from("smooth"), suffix: String::from("ly") },
-            StemmedWord { stemmed_offset: 33, suffix_offset: 37, suffix_len: 0,
+            StemmedWord { stemmed_offset: 33, suffix_offset: 37,
                           stemmed: String::from("that"), suffix: String::from("") },
-            StemmedWord { stemmed_offset: 38, suffix_offset: 42, suffix_len: 4,
+            StemmedWord { stemmed_offset: 38, suffix_offset: 42,
                           stemmed: String::from("stem"), suffix: String::from("ming") },
             ];
         assert_eq!(result.len(), expected.len());
@@ -136,20 +114,20 @@ mod tests {
         let stems = Stems::new(input);
         let result = Stems::new(input).collect::<Vec<StemmedWord>>();
         let expected = vec![
-            StemmedWord { stemmed_offset: 0, suffix_offset: 5, suffix_len: 0,
+            StemmedWord { stemmed_offset: 0, suffix_offset: 5,
                           stemmed: String::from("these"), suffix: String::from("") },
-            StemmedWord { stemmed_offset: 6, suffix_offset: 10, suffix_len: 1,
+            StemmedWord { stemmed_offset: 6, suffix_offset: 10,
                           stemmed: String::from("word"), suffix: String::from("s") },
             // "deeply" stems to "deepli"
-            StemmedWord { stemmed_offset: 12, suffix_offset: 17, suffix_len: 1,
+            StemmedWord { stemmed_offset: 12, suffix_offset: 17,
                           stemmed: String::from("deepli"), suffix: String::from("y") },
-            StemmedWord { stemmed_offset: 19, suffix_offset: 23, suffix_len: 0,
+            StemmedWord { stemmed_offset: 19, suffix_offset: 23,
                           stemmed: String::from("test"), suffix: String::from("") },
-            StemmedWord { stemmed_offset: 24, suffix_offset: 30, suffix_len: 2,
+            StemmedWord { stemmed_offset: 24, suffix_offset: 30,
                           stemmed: String::from("smooth"), suffix: String::from("ly") },
-            StemmedWord { stemmed_offset: 33, suffix_offset: 37, suffix_len: 0,
+            StemmedWord { stemmed_offset: 33, suffix_offset: 37,
                           stemmed: String::from("that"), suffix: String::from("") },
-            StemmedWord { stemmed_offset: 38, suffix_offset: 42, suffix_len: 4,
+            StemmedWord { stemmed_offset: 38, suffix_offset: 42,
                           stemmed: String::from("stem"), suffix: String::from("ming") },
             ];
         assert_eq!(result.len(), expected.len());
@@ -160,7 +138,7 @@ mod tests {
 
     #[test]
     fn test_stems_nonchars() {
-        let input = "  1234 \t89";
+        let input = "  @#$!== \t+-";
         let stems = Stems::new(input);
         let result = Stems::new(input).collect::<Vec<StemmedWord>>();
         assert_eq!(result.len(), 0);
@@ -168,14 +146,12 @@ mod tests {
 
     #[test]
     fn test_stems_some_nonchars() {
-        let input = "534   Let's seeing89";
+        let input = "@!?   Let's seeing...";
         let result = Stems::new(input).collect::<Vec<StemmedWord>>();
         let expected = vec![
-            StemmedWord { stemmed_offset: 6, suffix_offset: 9, suffix_len: 0,
-                          stemmed: String::from("let"), suffix: String::from("") },
-            StemmedWord { stemmed_offset: 10, suffix_offset: 11, suffix_len: 0,
-                          stemmed: String::from("s"), suffix: String::from("") },
-            StemmedWord { stemmed_offset: 12, suffix_offset: 15, suffix_len: 3,
+            StemmedWord { stemmed_offset: 6, suffix_offset: 9,
+                          stemmed: String::from("let"), suffix: String::from("'s") },
+            StemmedWord { stemmed_offset: 12, suffix_offset: 15,
                           stemmed: String::from("see"), suffix: String::from("ing") },
             ];
         assert_eq!(result.len(), expected.len());
@@ -189,9 +165,9 @@ mod tests {
         let input = "Ünicöde stemming";
         let result = Stems::new(input).collect::<Vec<StemmedWord>>();
         let expected = vec![
-            StemmedWord { stemmed_offset: 0, suffix_offset: 8, suffix_len: 1,
+            StemmedWord { stemmed_offset: 0, suffix_offset: 8,
                           stemmed: String::from("ünicöd"), suffix: String::from("e") },
-            StemmedWord { stemmed_offset: 10, suffix_offset: 14, suffix_len: 4,
+            StemmedWord { stemmed_offset: 10, suffix_offset: 14,
                           stemmed: String::from("stem"), suffix: String::from("ming") },
             ];
         assert_eq!(result.len(), expected.len());
@@ -203,14 +179,10 @@ mod tests {
     #[test]
     fn test_stems_unicode_lowercase_has_more_bytes() {
         let input = "İ";
-        println!("{} {} {} {}", input, input.to_lowercase(), input.len(), input.to_lowercase().len());
         let stems = Stems::new(input);
-        for stemmed in stems {
-            println!("stemmed: {:?}", stemmed);
-        }
         let result = Stems::new(input).collect::<Vec<StemmedWord>>();
         let expected = vec![
-            StemmedWord { stemmed_offset: 0, suffix_offset: 3, suffix_len: 0,
+            StemmedWord { stemmed_offset: 0, suffix_offset: 3,
                           stemmed: String::from("i̇"), suffix: String::from("") },
             ];
         assert_eq!(result.len(), expected.len());
@@ -219,15 +191,15 @@ mod tests {
         }
     }
 
-    // NOTE vmx 2016-10-21: Those cases exist in theory, but (language independent) it doesn't
-    // seem to be the case (after reading
-    // ftp://ftp.unicode.org/Public/UCD/latest/ucd/SpecialCasing.txt ). Some characters don't have
-    // a corresponding uppercase chararacter with the same byte size, but when those uppercase
-    // letters are transformed, to lowercase, they are transformed to the same number of bytes
-    // combining several characters (although there is a single character for it).
+    // Some characters don't have a corresponding uppercase chararacter with the same byte size,
+    // but when those uppercase letters are transformed to lowercase, they are transformed to the
+    // same number of bytes combining several characters (although there is a single character for
+    // it).
     // Here's an example in Python:
     //
     //     >>> u"Ρ̓".lower() == u"ῤ"
+    //     False
+    //     >>> u"Ρ̓".lower() == u"ῤ".lower()
     //     False
     //     >>> u"Ρ̓".lower() == u"ῤ".upper().lower()
     //     True
@@ -239,9 +211,21 @@ mod tests {
     //    println!("lower({}) == lower(upper({})): {}", upper, lower, upper.to_lowercase() == lower.to_uppercase().to_lowercase());
     //    lower(Ρ̓) == ῤ: false
     //    lower(Ρ̓) == lower(upper(ῤ)): true
-    //#[test]
-    //fn test_stems_unicode_lowercase_has_less_bytes() {
-    //}
+    #[test]
+    fn test_stems_unicode_lowercase_has_less_bytes() {
+        // The input is: Ρ̓ῤῤ (11 bytes), lowercases is ῤῤῤ (9 bytes)
+        let input = "\u{03A1}\u{0313}\u{03C1}\u{0313}\u{1FE4}";
+        let result = Stems::new(input).collect::<Vec<StemmedWord>>();
+        let stemmed = Stems::new(input);
+        let expected = vec![
+            StemmedWord { stemmed_offset: 0, suffix_offset: 9,
+                          stemmed: String::from("\u{1FE4}\u{1FE4}\u{1FE4}"), suffix: String::from("") },
+           ];
+        assert_eq!(result.len(), expected.len());
+        for (stem, expected_stem) in result.iter().zip(expected.iter()) {
+            assert_eq!(stem, expected_stem);
+        }
+    }
 
     #[test]
     fn test_common_prefix_len() {
