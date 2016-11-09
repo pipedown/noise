b6aa2a593848530a3a68aa85991dafd9b2520689
Volker Mische <volker.mische@gmail.com>
2016-11-08 19:43:41 -0800

    First query works

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+extern crate noise;
+
+use noise::index::{Index, OpenOptions};
+use noise::query::Query;
+
+fn main() {
+    let dbname = "querytestdb";
+    Index::delete(dbname);
+
+    let mut index = Index::new();
+    index.open(dbname, Some(OpenOptions::Create)).unwrap();
+    index.add(r#"{"_id": "foo", "hello": "world"}"#);
+    index.flush().unwrap();
+
+    let mut query_results = Query::get_matches(r#"hello="world""#.to_string(), &index).unwrap();
+    //let mut query_results = Query::get_matches(r#"a.b[foo="bar"]"#.to_string(), &index).unwrap();
+    println!("query results: {:?}", query_results.get_next_id());
+}
